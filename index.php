1695565982ef33fb72fc36008df6b1c5fcd58c21
Hdocwra <hdocwra@gmail.com>
2016-02-26 15:15:39 +0000

    Added 'some' content.

--- index.php
+++ index.php
@@ -2,15 +2,32 @@
 <head>
     <title>Ingredial</title>
 
-    <link href="css/bootstrap.min.css" type="text/css" />
-    <link href="css/magicsuggest-min.css" type="text/css" />
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/magicsuggest-min.css" rel="stylesheet">
 </head>
 <body>
 
-<h1>Hello world.</h1>
+<div class="container">
+    <div class="row">
+    <h1>Hello world.</h1>
+         <div class="form-group">
+            <label>Scrabble box</label>
+            <input class="form-control" id="ms-scrabble">
+        </div>
+    </div>
+</div>
 
 <script src="js/jquery-2.2.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/magicsuggest-min.js"></script>
+
+<script>
+    $(function() {
+        $('#ms-scrabble').magicSuggest({
+            placeholder: 'Type some real or fake fruits',
+            data: ['Banana', 'Apple', 'Orange', 'Lemon']
+        });
+    });
+</script>
 </body>
 </html>
