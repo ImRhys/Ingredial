e6999f5e3fce65c2a78e41addcf1f48f6ad6fc85
Hdocwra <hdocwra@gmail.com>
2016-04-01 13:29:15 +0100

    Added buttons, made things pretty

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <html>
 <head>
-    <title>Ingredial</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/magicsuggest-min.css" rel="stylesheet">
-    <link href="css/override.css" rel="stylesheet">
+	<title>Ingredial</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/magicsuggest-min.css" rel="stylesheet">
+	<link href="css/override.css" rel="stylesheet">
 </head>
 <body>
 
 <div class="container">
-    <div class="col-sm-6 col-centered offset-top">
-        <img src="img/logo.png" class="img-responsive" id="main-logo" alt="Main logo" />
-         <div class="form-group">
-            <input class="form-control" id="ms-scrabble">
+	<div class="col-sm-6 col-centered offset-top">
+		<img src="img/logo.png" class="img-responsive" id="main-logo" alt="Main logo" />
+        <div class="form-group">
+            <form id="searchf" method="get" action="">
+                <input class="form-control" id="ms-scrabble">
+                <div class="center-block main-buttons-group">
+                    <button type="button" class="btn btn-default main-buttons" onclick="" style="margin-right: 5px">Search</button>
+                    <button type="button" class="btn btn-default main-buttons" onclick="">Surprise Me</button>
+                </div>
+            </form>
         </div>
-    </div>
+	</div>
 </div>
 
 <script src="js/jquery-2.2.1.min.js"></script>
@@ -21,12 +27,16 @@
 <script src="js/magicsuggest-min.js"></script>
 
 <script>
-    $(function() {
-        $('#ms-scrabble').magicSuggest({
-            placeholder: 'Type some real or fake fruits',
-            data: ['Banana', 'Apple', 'Orange', 'Lemon']
-        });
-    });
+	$(function() {
+		$('#ms-scrabble').magicSuggest({
+			placeholder: 'Type some real or fake fruits',
+			data: ['Banana', 'Apple', 'Orange', 'Lemon']
+		});
+	});
+
+    function submitf() {
+        $('#searchf').submit();
+    }
 </script>
 </body>
 </html>
