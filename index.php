14b13c38bb270d0bfb80e4c2775b7818cb985c7b
Hdocwra <hdocwra@gmail.com>
2016-04-22 15:19:24 +0100

    Fix JS code block.

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
 	<div class="col-sm-6 col-centered offset-top">
 		<img src="img/logo.png" class="img-responsive" id="main-logo" alt="Main logo" />
         <div class="form-group">
-            <form id="searchf" method="get" action="">
+            <form id="searchf" method="get" action="" onsubmit="return false;">
                 <input class="form-control" id="ms-scrabble">
                 <div class="center-block main-buttons-group">
-                    <button type="button" class="btn btn-default main-buttons" onclick="" style="margin-right: 5px">Search</button>
-                    <button type="button" class="btn btn-default main-buttons" onclick="">Surprise Me</button>
+                    <button type="button" class="btn btn-default main-buttons" id="submit" style="margin-right: 5px">Search</button>
+                    <button type="button" class="btn btn-default main-buttons" id="randomr">Surprise Me</button>
                 </div>
             </form>
         </div>
@@ -32,15 +32,61 @@
 
 <script>
 	$(function() {
-		$('#ms-scrabble').magicSuggest({
-			placeholder: 'What do you have to hand?',
-			data: ['Banana', 'Apple', 'Orange', 'Lemon']
-		});
-	});
 
-    function submitf() {
-        $('#searchf').submit();
-    }
+        var apiKey = "";
+
+		var ms = $('#ms-scrabble').magicSuggest({
+			placeholder: 'What do you have to hand?',
+			data: [],
+            required: true
+		});
+
+        function getRecipe(RecipeId) {
+            var url = "httpss://api2.bigoven.com/recipe/" + RecipeId + "?api_key=" + apiKey;
+            $.ajax({
+                type: "GET",
+                dataType: 'json',
+                cache: false,
+                url: url,
+                success: function (data) {
+                    console.log(data);
+                    //$("#RecipeTitle").html(data.Title);
+                    //$("#instructions").html(data.Instructions);
+                }
+            });
+        }
+
+        function getSuggestion(CurrentStr) {
+            var url = "httpss://api2.bigoven.com/autocomplete?limit=10&query=" + CurrentStr + "&api_key=" + apiKey;
+            $.ajax({
+                type: "GET",
+                dataType: 'json',
+                cache: false,
+                url: url,
+                success: function (data) {
+                    console.log(data);
+                    ms.setData(data);
+                }
+            });
+        }
+
+        function submitf() {
+            alert(JSON.stringify(ms.getValue()));
+        }
+
+        $('#submit').click(function() {
+            submitf();
+        });
+
+        $('#randomr').click(function() {
+            alert("Random recipe!") //todo
+        });
+
+        $(ms).on('keyup', function(e, m, v){
+            //alert('Key code # ' + v.keyCode);
+            getSuggestion(ms.getValue()[0]);
+        });
+	});
 </script>
 </body>
 </html>
