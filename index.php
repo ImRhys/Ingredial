7ccd45484495a77eb00468473e974f9661c7d439
hdocwra <hdocwra@gmail.com>
2016-04-27 20:48:04 +0100

    Derp #3

--- index.php
+++ index.php
@@ -113,7 +113,7 @@
     }
 
     function getSuggestion(CurrentStr) {
-      var url = "http://ingredial.azurewebsites.net/theprox.php?url=https://api2.bigoven.com/recipes/autocomplete&query=" + CurrentStr + "&limit=10&api_key=" + apiKey;
+      var url = "http://ingredial.azurewebsites.net/theprox.php?url=https://api2.bigoven.com/recipe/autocomplete&query=" + CurrentStr + "&limit=10&api_key=" + apiKey;
       $.ajax({
         type: "GET",
         dataType: 'json',
