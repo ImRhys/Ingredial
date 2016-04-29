e432375630428878c545482f372c0fb0a1ccac35
Hannon Docwra <hdocwra@gmail.com>
2016-04-29 09:31:29 +0100

    added debugging line

--- theprox.php
+++ theprox.php
@@ -2,17 +2,24 @@
 $url = $_GET['url'];
 $parameters = "";
 foreach($_GET as $key => $value){
-  if($key != "url") {
+  if($key != "url" || $key != "debug") {
     $parameters = $parameters . "&" . $key . "=" . $value;
   }
 }
 $parameters = substr($parameters, 1);
-$finalurl = $url . "?" . $parameters;
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, $finalurl);
-$result = curl_exec($ch);
-curl_close($ch);
-header('Content-Type: application/json');
-echo $result;
+
+if (isset($_GET['debug'])) {
+  echo "<pre>";
+  print_r($parameters);
+  echo "</pre>";
+} else {
+  $finalurl = $url . "?" . $parameters;
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_URL, $finalurl);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  header('Content-Type: application/json');
+  echo $result;
+}
